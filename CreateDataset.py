5432720ed1ebd62483469ce658d97b652046ad66
Daniele Pellegrini <daniele.pellegrini@studenti.unipr.it>
2021-09-03 17:35:32 +0200

    Update CreateDataset.py
    Add input parameters from command line to create the Dataset. Add datasets with different parameters.

--- CreateDataset.py
+++ CreateDataset.py
@@ -2,8 +2,10 @@ from random import randint
 from itertools import permutations
 from csv import writer
 from scipy.spatial import distance_matrix
+from string import ascii_uppercase
 
 import numpy as np
+import argparse
 import os
 
 """ Read the Coordinates and calculate the Distance Matrix from a file.tsp
@@ -44,24 +46,37 @@ def get_solution_from_file(FOLDER_NAME, file_name):
 
     return tour_length, tour
 
-def create_csv_file(FOLDER_NAME, file_name):
+""" Create the CSV dataset file
+"""
+def create_csv_file(FOLDER_NAME, N_RECORDS, N_SEQUENCE):
     if not os.path.exists(FOLDER_NAME):
         os.makedirs(FOLDER_NAME)
     else:
-        if os.path.exists(f'{FOLDER_NAME}/{file_name}.csv'):
-            os.remove(f'{FOLDER_NAME}/{file_name}.csv')
-    columns = [ 'A_x', 'A_y', 'B_x', 'B_y', 
-                'C_x', 'C_y', 'D_x', 'D_y', 
-                'E_x', 'E_y', 'AB', 'BC', 
-                'CD', 'DE', 'Length', 'Good' ]
-    with open(f'{FOLDER_NAME}/{file_name}.csv', 'w', newline='') as csv_file:
+        if os.path.exists(f'{FOLDER_NAME}/Dataset_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}.csv'):
+            os.remove(f'{FOLDER_NAME}/Dataset_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}.csv')
+    alphabet = list(ascii_uppercase)
+    columns = []
+    
+    for l in range(0, N_SEQUENCE):
+        columns.append(f'{alphabet[l]}_x')
+        columns.append(f'{alphabet[l]}_y')
+    
+    for l in range(0, N_SEQUENCE-1):
+        columns.append(f'{alphabet[l]}{alphabet[l+1]}')
+    
+    columns.append('Length')
+    columns.append('Good')
+
+    with open(f'{FOLDER_NAME}/Dataset_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}.csv', 'w', newline='') as csv_file:
         # creating a csv writer object 
         csvwriter = writer(csv_file)
         # writing the fields 
         csvwriter.writerow(columns)
 
-def add_record_to_csv(FOLDER_NAME, file_name, record):
-    with open(f'{FOLDER_NAME}/{file_name}.csv', 'a', newline='') as csv_file:
+""" Add a record in the CSV dataset file
+"""
+def add_record_to_csv(FOLDER_NAME, N_RECORDS, N_SEQUENCE, record):
+    with open(f'{FOLDER_NAME}/Dataset_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}.csv', 'a', newline='') as csv_file:
         # Pass this file object to csv.writer()
         # and get a writer object
         writer_object = writer(csv_file)
@@ -70,10 +85,27 @@ def add_record_to_csv(FOLDER_NAME, file_name, record):
         # the writerow()
         writer_object.writerow(record)
 
+""" Create the Record to insert in the Dataset
+"""
+def create_record(sequence, coords, distance_matrix, output):
+    record = []
+    for node in sequence:
+        record.append(coords[node-1][0])
+        record.append(coords[node-1][1])
+    for subtour in zip(sequence, sequence[1:]):
+        record.append(get_sequence_length(subtour, distance_matrix))
+    record.append(get_sequence_length(sequence, distance_matrix))
+    record.append(output)
+    return record
+
+def split_csv(FOLDER_NAME, file_name):
+    ...
+
 """ Create the Dataset
 """
-def create_dataset(TSP_FOLDER, DATASET_FOLDER, dataset_name, N_RECORDS):
-    create_csv_file(DATASET_FOLDER, dataset_name)
+def create_dataset(TSP_FOLDER, DATASET_FOLDER, N_RECORDS, N_SEQUENCE):
+    n_samples = N_RECORDS//4 
+    create_csv_file(DATASET_FOLDER, N_RECORDS, N_SEQUENCE)
     for i in range(1, 101):
         file_name = f'problem_{i}'
         coords, distance_matrix = get_coordinates_from_file(TSP_FOLDER, file_name)
@@ -83,30 +115,30 @@ def create_dataset(TSP_FOLDER, DATASET_FOLDER, dataset_name, N_RECORDS):
         sorted_errors = get_sorted_errors(tour, tour_length, distance_matrix)
         print(f'SORTED ERRORS: {sorted_errors}\n\n')
         
-        worst_indexes = [ x for x in list(sorted_errors)[:5] ]
-        best_indexes = [ x for x in list(sorted_errors)[-5:] ] 
+        worst_indexes = [ x for x in list(sorted_errors)[:n_samples] ]
+        best_indexes = [ x for x in list(sorted_errors)[-n_samples:] ] 
         #print(f'Best Indexes: {best_indexes} \nWorst Indexes: {worst_indexes}')
         
-        best_sequences_list = get_best_sequences(tour)
-        good_sequences_list = get_other_sequences(tour, best_indexes)
-        bad_sequences_list = get_other_sequences(tour, worst_indexes)
-        worst_sequences = get_worst_sequences(tour, worst_indexes, distance_matrix)
+        best_sequences_list = get_best_sequences(tour, n_samples, N_SEQUENCE)
+        good_sequences_list = get_other_sequences(tour, best_indexes, n_samples, N_SEQUENCE)
+        bad_sequences_list = get_other_sequences(tour, worst_indexes, n_samples, N_SEQUENCE)
+        worst_sequences = get_worst_sequences(tour, worst_indexes, distance_matrix, N_SEQUENCE)
         
         for seq in best_sequences_list:
             record = create_record(seq, coords, distance_matrix, 1)
-            add_record_to_csv(DATASET_FOLDER, dataset_name, record)
+            add_record_to_csv(DATASET_FOLDER, N_RECORDS, N_SEQUENCE, record)
 
         for seq in good_sequences_list:
             record = create_record(seq, coords, distance_matrix, 1)
-            add_record_to_csv(DATASET_FOLDER, dataset_name, record)
+            add_record_to_csv(DATASET_FOLDER, N_RECORDS, N_SEQUENCE, record)
         
         for seq in bad_sequences_list:
             record = create_record(seq, coords, distance_matrix, 0)
-            add_record_to_csv(DATASET_FOLDER, dataset_name, record)
+            add_record_to_csv(DATASET_FOLDER, N_RECORDS, N_SEQUENCE, record)
         
         for seq in worst_sequences:
             record = create_record(seq, coords, distance_matrix, 0)
-            add_record_to_csv(DATASET_FOLDER, dataset_name, record)
+            add_record_to_csv(DATASET_FOLDER, N_RECORDS, N_SEQUENCE, record)
 
     """ TODO 
                 (1)
@@ -125,29 +157,24 @@ def create_dataset(TSP_FOLDER, DATASET_FOLDER, dataset_name, N_RECORDS):
                 - Testare algoritmo di ML con nuovi file.tsp e file.tour generati casualmente
 
     """
-    ...
 
-def create_record(sequence, coords, distance_matrix, output):
-    record = []
-    for node in sequence:
-        record.append(coords[node-1][0])
-        record.append(coords[node-1][1])
-    for subtour in zip(sequence, sequence[1:]):
-        record.append(get_sequence_length(subtour, distance_matrix))
-    record.append(get_sequence_length(sequence, distance_matrix))
-    record.append(output)
-    return record
+""" Get the Best Sequences of a Tour
 
-def get_best_sequences(best_tour, n=5):
+    Return the best sequences in the best tour known, choosing them randomly.
+        - best_tour : the optimal tour known
+        - n_samples : number of samples to insert in the sequences list
+        - N_SEQUENCE : the number of nodes in the sequence 
+"""
+def get_best_sequences(best_tour, n_samples, N_SEQUENCE):
     random_indexes_list = []
     sequences_list = []
-    while len(sequences_list) < n:
+    while len(sequences_list) < n_samples:
         matching_node = False
         random_node = randint(0,100)
-        sequence = get_sequence(best_tour, random_node)
+        sequence = get_sequence(best_tour, random_node, N_SEQUENCE)
         #print(f'Node: {random_node}, Sequence: {sequence}')
                 
-        # Check if the generated node already belongs to a 
+        # Check if the generated node already already belongs to a sequence
         for node in sequence:
             for seq in sequences_list:
                 if node in seq:
@@ -162,24 +189,42 @@ def get_best_sequences(best_tour, n=5):
     
     return sequences_list
 
-def get_other_sequences(tour, indexes_list):
+""" Get the other Sequences obtained by doing some exchanges.
+
+    Return the sequences obtained by by exchanging the nodes corresponding to the indexes in the list given.
+    Each node it's exchanged with its one following.
+        - tour : the array containing the nodes ordered by visit
+        - indexes_list : the list containg the indexes of a tour
+        - n_samples : number of samples to insert in the sequences list
+        - N_SEQUENCE : the number of nodes in the sequence
+"""
+def get_other_sequences(tour, indexes_list, n_samples, N_SEQUENCE):
     sequences_list = []
-    for i in range(5):
+    for i in range(n_samples):
         new_tour = tour.copy()
         # Exchange the i-node with its next one
         new_tour[indexes_list[i]] = tour[(indexes_list[i]+1) % len(tour)]
         new_tour[(indexes_list[i]+1) % len(tour)] = tour[indexes_list[i]]
-        new_sequence = get_sequence(new_tour, indexes_list[i])
+        new_sequence = get_sequence(new_tour, indexes_list[i], N_SEQUENCE)
         #print(f'Index: {indexes_list[i]}, New Sequence: {new_sequence}')
         sequences_list.append(new_sequence)
     return sequences_list
 
-def get_worst_sequences(tour, worst_indexes, distance_matrix):
-    # [[seq1], [seq2], ..., [seq5]]
+""" Get the Worst Sequences on a Graph by doing some permutations.
+
+    Return the worst sequence by doing all the permutations for each worst_index given. The worst sequence 
+    is the one which has a major error from the original one belonging to the tour. The sequence compared
+    must have the same beginning node and the same ending node.
+        - tour : the array containing the nodes ordered by visit
+        - worst_index : the list containg the worst indexes of a tour
+        - distance_matrix : the matrix containing the distances of each node
+        - N_SEQUENCE : the number of nodes in the sequence
+"""
+def get_worst_sequences(tour, worst_indexes, distance_matrix, N_SEQUENCE):
     error_dict = {}
-    worst_sequences = []
+    worst_sequences = [] # [[seq1], [seq2], ..., [seq5]]
     for index in worst_indexes:
-        sequence = get_sequence(tour, index)
+        sequence = get_sequence(tour, index, N_SEQUENCE)
         sequence_length = get_sequence_length(sequence, distance_matrix)
         permutation_tuple = list(permutations(node for node in sequence[1:-1]))
         permutation_list = [list(i) for i in permutation_tuple]
@@ -204,31 +249,45 @@ def get_worst_sequences(tour, worst_indexes, distance_matrix):
 
     return worst_sequences
 
+""" Create a New Tour replacing a Sequence
+
+    Return a new tour replacing a sequence in the given one.
+        - tour : the array containing the nodes ordered by visit
+        - index : the index of the node to take inside the sequence
+        - N_SEQUENCE : the number of nodes in the sequence
+"""
 def create_new_tour(tour, index, sequence):
     #print(f'TOUR: {tour}')
+    n_sequence = len(sequence)
     print(f'Index: {index}, Sequence to Replace: {sequence}')
     new_tour = tour.copy()
-    new_tour[index] = sequence[2]
-    new_tour[(index-1)%len(tour)] = sequence[1]
-    new_tour[(index+1)%len(tour)] = sequence[3]
+    # Even Sequence
+    if n_sequence % 2 == 0:
+        for idx, i in enumerate(range(-(n_sequence//2)+1, (n_sequence//2)+1)):
+            new_tour[(index+i)%len(tour)] = sequence[idx]
+    # Odd Sequence
+    else:
+        for idx, i in enumerate(range(-(n_sequence//2), (n_sequence//2)+1)):
+            new_tour[(index+i)%len(tour)] = sequence[idx]
     #print(f'New Tour: {new_tour}')
+
     return new_tour
 
 """ Return a Sequence of n nodes 
 
     Return a sequence of n nodes in the given tour.
         - tour : the array containing the nodes ordered by visit
-        - candidate : the index of the node to take inside the sequence
+        - index : the index of the node to take inside the sequence
+        - N_SEQUENCE : the number of nodes in the sequence
     Default value for n is 5. A sequence of 5 nodes is returned. If n is even, the first
-    (n/2) nodes before the candidate are taken with the last (n/2) following it.
-    Otherwise, the first (n/2)-1 nodes before the candidate are taken with the last (n/2) following it.
+    (n/2) nodes before the index are taken with the last (n/2) following it.
+    Otherwise, the first (n/2)-1 nodes before the index are taken with the last (n/2) following it.
 """
-def get_sequence(tour, candidate, n=5):
-    if n % 2 == 0:
-        return [tour[i%len(tour)] for i in range(candidate-(n//2)+1, candidate+(n//2)+1)]
+def get_sequence(tour, index, N_SEQUENCE):
+    if N_SEQUENCE % 2 == 0:
+        return [tour[i%len(tour)] for i in range(index-(N_SEQUENCE//2)+1, index+(N_SEQUENCE//2)+1)]
     else:
-        return [tour[i%len(tour)] for i in range(candidate-(n//2), candidate+(n//2)+1)]
-
+        return [tour[i%len(tour)] for i in range(index-(N_SEQUENCE//2), index+(N_SEQUENCE//2)+1)]
     
 """
     Return the errors of the new tours calculated exchanging each node of the tour by one position.
@@ -284,16 +343,39 @@ def get_sequence_length(sequence, distance_matrix):
 def get_tour_error(best_tour_length, tour_length):
     return round(((tour_length - best_tour_length)/tour_length)*100, 2)
 
+""" Validate the Inputs
+"""
+def validate_input(N_SEQUENCE, N_RECORDS):
+    try:
+        assert N_RECORDS % 4 == 0 and 4 <= N_SEQUENCE <= 8
+    except AssertionError as error:
+        if (N_RECORDS % 4) != 0:
+            print(f'Invalid Number of Records: {N_RECORDS}. Please insert Number of Records divisible by 4.')
+        if 4 <= N_SEQUENCE <= 8:
+            print(f'Invalid Number of Nodes in a Sequence: {N_SEQUENCE}. Please consider a number of Nodes between 4 and 8.')
+        raise
+
 def main():
     TSP_FOLDER = "./TSPRandomGraph"
     DATASET_FOLDER = './Dataset'
-    dataset_name = 'Dataset'
-    SEQUENCE_DIMENSION = 5
-    N_RECORDS = 20
-    create_dataset(TSP_FOLDER, DATASET_FOLDER, dataset_name, N_RECORDS)
-    # s = [75, 98, 1, 96, 22]
-    # create_new_tour(tour, 0, s)
 
+    # Default Configuration if no args are given
+    N_RECORDS = 40
+    N_SEQUENCE = 5
+    parser = argparse.ArgumentParser(description='Dataset Configuration')
+    parser.add_argument('--r', dest='record', type=int, help='Record to Save for each Graph')
+    parser.add_argument('--s', dest='sequence_nodes', type=int, help='Nodes in each sequence')
+    args = parser.parse_args()
+    # Set the Configuration Parameter
+    if args.record is not None:
+        N_RECORDS = args.record
+    if args.sequence_nodes is not None:
+        N_SEQUENCE = args.sequence_nodes
+    # Check if the Parameters are correct
+    validate_input(N_SEQUENCE, N_RECORDS)
+
+    print(f'Number of Records for each Graph: {N_RECORDS}, Number of Nodes in a Sequence: {N_SEQUENCE}')
+    create_dataset(TSP_FOLDER, DATASET_FOLDER, N_RECORDS, N_SEQUENCE)
 
 if __name__ == "__main__":
     main()    
