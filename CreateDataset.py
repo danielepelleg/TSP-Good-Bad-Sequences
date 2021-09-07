72524812cccb955fc8bb0c65be9bafb132f2e5cf
Daniele Pellegrini <daniele.pellegrini@studenti.unipr.it>
2021-09-08 01:34:30 +0200

    Update CreateDataset.py

--- CreateDataset.py
+++ CreateDataset.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import seed, randint, shuffle
 from itertools import permutations
 from csv import writer
 from scipy.spatial import distance_matrix
@@ -77,12 +77,10 @@ def create_csv_file(FOLDER_NAME, N_RECORDS, N_SEQUENCE):
 """
 def add_record_to_csv(FOLDER_NAME, N_RECORDS, N_SEQUENCE, record):
     with open(f'{FOLDER_NAME}/Dataset_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}.csv', 'a', newline='') as csv_file:
-        # Pass this file object to csv.writer()
-        # and get a writer object
+        # Pass this file object to csv.writer() and get a writer object
         writer_object = writer(csv_file)
     
-        # Pass the list as an argument into
-        # the writerow()
+        # Pass the list as an argument into the writerow()
         writer_object.writerow(record)
 
 """ Create the Record to insert in the Dataset
@@ -95,34 +93,41 @@ def create_record(sequence, coords, distance_matrix, output):
     for subtour in zip(sequence, sequence[1:]):
         record.append(get_sequence_length(subtour, distance_matrix))
     record.append(get_sequence_length(sequence, distance_matrix))
-    record.append(output)
+    if output is not None:
+        record.append(output)
     return record
-
-def split_csv(FOLDER_NAME, file_name):
-    ...
 
 """ Create the Dataset
 """
-def create_dataset(TSP_FOLDER, DATASET_FOLDER, N_RECORDS, N_SEQUENCE):
+def create_dataset(TSP_FOLDER, DATASET_FOLDER, N_RECORDS, N_SEQUENCE, ERROR_TARGET):
     n_samples = N_RECORDS//4 
     create_csv_file(DATASET_FOLDER, N_RECORDS, N_SEQUENCE)
-    for i in range(1, 101):
+    for i in range(1, 201):
         file_name = f'problem_{i}'
+        print(f'Problem: {file_name}\n')
         coords, distance_matrix = get_coordinates_from_file(TSP_FOLDER, file_name)
         tour_length, tour = get_solution_from_file(TSP_FOLDER, file_name)
-        print(f'Problem: {file_name} \nTour Length: {tour_length} \nTOUR: {tour}')
-        
+        random_tour = get_random_tour(tour_length, distance_matrix)
+        #print(f'Tour Length: {tour_length} \nTOUR: {tour}')
+    
         sorted_errors = get_sorted_errors(tour, tour_length, distance_matrix)
-        print(f'SORTED ERRORS: {sorted_errors}\n\n')
+        #print(f'SORTED ERRORS: {sorted_errors}\n\n')
         
         worst_indexes = [ x for x in list(sorted_errors)[:n_samples] ]
         best_indexes = [ x for x in list(sorted_errors)[-n_samples:] ] 
         #print(f'Best Indexes: {best_indexes} \nWorst Indexes: {worst_indexes}')
         
-        best_sequences_list = get_best_sequences(tour, n_samples, N_SEQUENCE)
-        good_sequences_list = get_other_sequences(tour, best_indexes, n_samples, N_SEQUENCE)
-        bad_sequences_list = get_other_sequences(tour, worst_indexes, n_samples, N_SEQUENCE)
-        worst_sequences = get_worst_sequences(tour, worst_indexes, distance_matrix, N_SEQUENCE)
+        best_sequences_list = get_unique_sequences(tour, n_samples, N_SEQUENCE)
+        #print(f'Best Sequences: {best_sequences_list}\n')
+        good_sequences_list = get_good_sequences(tour, best_indexes, n_samples, N_SEQUENCE)
+        #print(f'Good Sequences: {good_sequences_list}\n')
+        bad_sequences_list = get_bad_sequences(tour, worst_indexes, distance_matrix, N_SEQUENCE, ERROR_TARGET)
+        # Additional samples to add as a worst sequence in the dataset 
+        # if not enough bad sequences are found
+        additional_samples = n_samples - len(bad_sequences_list)
+        #print(f'Bad Sequences: {bad_sequences_list}\tAdditional Samples: {additional_samples}\n')
+        worst_sequences_list = get_random_sequences(random_tour, n_samples + additional_samples, N_SEQUENCE)
+        #print(f'Worst Sequences: {worst_sequences_list}\n')
         
         for seq in best_sequences_list:
             record = create_record(seq, coords, distance_matrix, 1)
@@ -136,56 +141,84 @@ def create_dataset(TSP_FOLDER, DATASET_FOLDER, N_RECORDS, N_SEQUENCE):
             record = create_record(seq, coords, distance_matrix, 0)
             add_record_to_csv(DATASET_FOLDER, N_RECORDS, N_SEQUENCE, record)
         
-        for seq in worst_sequences:
+        for seq in worst_sequences_list:
             record = create_record(seq, coords, distance_matrix, 0)
             add_record_to_csv(DATASET_FOLDER, N_RECORDS, N_SEQUENCE, record)
 
-    """ TODO 
-                (1)
-                - Serializzare il dataset
-                    - coordinate
-                    - distanze tra nodi (archi)
-                    - lunghezza percorso
+""" Create a Random Tour
+"""
+def get_random_tour(best_tour_length, dist_mat):
+    TARGET_ERROR_RANDOM_TOUR = 15
+    random_tour = [i for i in range(1,101)]
+    valid = False
+    while not valid:
+        shuffle(random_tour)
+        random_tour_length = get_tour_length(random_tour, dist_mat)
+        error = get_tour_error(best_tour_length, random_tour_length)
+        if error >= TARGET_ERROR_RANDOM_TOUR:
+            valid = True
+    return random_tour
 
-                (2)
-                - Generalizzare creazione del dataset per N_RECORD 
-                - Utilizzare libreria generica per ottenere algoritmo di ML
-                - Testare algoritmo ed eventualmente ampliare il dataset aumentando N_RECORD, sequenze
-                    salvate oppure generando sequenze casuali
+""" Get Unique Sequences from a Tour
 
-                (3)
-                - Testare algoritmo di ML con nuovi file.tsp e file.tour generati casualmente
-
-    """
-
-""" Get the Best Sequences of a Tour
-
-    Return the best sequences in the best tour known, choosing them randomly.
-        - best_tour : the optimal tour known
+    Return a list of sequences from a tour known, choosing them so their nodes are unique.
+        - tour : the tour known
         - n_samples : number of samples to insert in the sequences list
         - N_SEQUENCE : the number of nodes in the sequence 
 """
-def get_best_sequences(best_tour, n_samples, N_SEQUENCE):
+def get_unique_sequences(tour, n_samples, N_SEQUENCE):
+    # The sequences are unique until the 70% of the nodes are taken
+    TARGET = int(len(tour)*0.7)
+    random_indexes_set = set()
+    sequences_list = []
+    random_node_index = randint(0,len(tour))
+    counter = 1
+    while len(sequences_list) < n_samples:
+        if n_samples * N_SEQUENCE <= TARGET:
+            if random_node_index not in random_indexes_set:
+                random_indexes_set.add(random_node_index)
+                # Add the N_SEQUENCE-1 neighbors of the random node chosen
+                for i in range(-(N_SEQUENCE-1),N_SEQUENCE):
+                    random_indexes_set.add((random_node_index+i)%len(tour))
+                sequence = get_sequence(tour, random_node_index, N_SEQUENCE)
+                sequences_list.append(sequence)
+            random_node_index = randint(0,len(tour))
+        else:
+            # If the seqeuences list is empty
+            if not sequences_list:
+                sequence = get_sequence(tour, random_node_index, N_SEQUENCE)
+                sequences_list.append(sequence)
+            # Take the previous and the next sequence and add them to the list
+            prev_sequence = get_sequence(tour, (random_node_index-(counter*N_SEQUENCE))%len(tour), N_SEQUENCE)
+            next_sequence = get_sequence(tour, (random_node_index+(counter*N_SEQUENCE))%len(tour), N_SEQUENCE)
+            sequences_list.append(prev_sequence)
+            sequences_list.append(next_sequence)
+            counter += 1
+    # If the list contains one more sequence
+    if len(sequences_list) > n_samples:
+        # Delete one random between the last two inserted
+        sequences_list.pop(randint(-2,0))
+    return sequences_list
+
+""" Get a Random Sequence from a Tour
+    
+    Return a list of sequences from a tour known, choosing them randomly.
+        - tour : the tour known
+        - n_samples : number of samples to insert in the sequences list
+        - N_SEQUENCE : the number of nodes in the sequence
+"""
+def get_random_sequences(tour, n_samples, N_SEQUENCE):
     random_indexes_list = []
     sequences_list = []
     while len(sequences_list) < n_samples:
-        matching_node = False
-        random_node = randint(0,100)
-        sequence = get_sequence(best_tour, random_node, N_SEQUENCE)
+        random_node = randint(0,len(tour))
+        if random_node in random_indexes_list:
+            continue
+        sequence = get_sequence(tour, random_node, N_SEQUENCE)
         #print(f'Node: {random_node}, Sequence: {sequence}')
-                
-        # Check if the generated node already already belongs to a sequence
-        for node in sequence:
-            for seq in sequences_list:
-                if node in seq:
-                    matching_node = True
-                    break
-            if matching_node: 
-                break  
-                
-        if not matching_node:
-            random_indexes_list.append(random_node)
-            sequences_list.append(sequence)
+        
+        random_indexes_list.append(i for i in range((random_node-1)%len(tour), (random_node+2)%len(tour)))
+        sequences_list.append(sequence)
     
     return sequences_list
 
@@ -198,7 +231,7 @@ def get_best_sequences(best_tour, n_samples, N_SEQUENCE):
         - n_samples : number of samples to insert in the sequences list
         - N_SEQUENCE : the number of nodes in the sequence
 """
-def get_other_sequences(tour, indexes_list, n_samples, N_SEQUENCE):
+def get_good_sequences(tour, indexes_list, n_samples, N_SEQUENCE):
     sequences_list = []
     for i in range(n_samples):
         new_tour = tour.copy()
@@ -210,42 +243,44 @@ def get_other_sequences(tour, indexes_list, n_samples, N_SEQUENCE):
         sequences_list.append(new_sequence)
     return sequences_list
 
-""" Get the Worst Sequences on a Graph by doing some permutations.
+""" Get Bad Sequences on a Graph by doing some permutations.
 
-    Return the worst sequence by doing all the permutations for each worst_index given. The worst sequence 
-    is the one which has a major error from the original one belonging to the tour. The sequence compared
+    Return the bad sequences by doing all the permutations for each worst_index given. A bad sequence 
+    is one which has a major error from the original one belonging to the tour. The sequence compared
     must have the same beginning node and the same ending node.
         - tour : the array containing the nodes ordered by visit
         - worst_index : the list containg the worst indexes of a tour
         - distance_matrix : the matrix containing the distances of each node
         - N_SEQUENCE : the number of nodes in the sequence
+        - ERROR_TARGET : the error to use to choose the sequence to discard
 """
-def get_worst_sequences(tour, worst_indexes, distance_matrix, N_SEQUENCE):
+def get_bad_sequences(tour, worst_indexes, distance_matrix, N_SEQUENCE, ERROR_TARGET):
     error_dict = {}
-    worst_sequences = [] # [[seq1], [seq2], ..., [seq5]]
     for index in worst_indexes:
-        sequence = get_sequence(tour, index, N_SEQUENCE)
+        sequence = get_sequence(tour, index, N_SEQUENCE) # [n1, n2, n3, n4, n5]
         sequence_length = get_sequence_length(sequence, distance_matrix)
+        # Permutate the nodes in the middle of the sequence [n1, n2, n3, n4, n5] -> [n2, n3, n4]
         permutation_tuple = list(permutations(node for node in sequence[1:-1]))
-        permutation_list = [list(i) for i in permutation_tuple]
-        permutation_list.pop(0)
+        permutation_list = [list(i) for i in permutation_tuple] # [ [n2, n3, n4], [n4, n2, n3], ...]
+        # Remove the sequence known from the permutation list
+        permutation_list.pop(0) # [[n4, n2, n3], ...]
 
-        for idx, bad_seq in enumerate(permutation_list):
+        for bad_seq in permutation_list:
+            # Add the two edge nodes to the sequence [n4, n2, n3] -> [n1, n4, n2, n3, n5]
             bad_seq.insert(0, sequence[0])
             bad_seq.insert(len(sequence), sequence[-1])
             bad_seq_length = get_sequence_length(bad_seq, distance_matrix)
-            error = get_tour_error(sequence_length, bad_seq_length)
-            error_dict[idx] = error
-            print(f'ORIGINAL SEQUENCE: {sequence} -> BAD SEQUENCE: {bad_seq}')
+            new_sequence_error = get_tour_error(sequence_length, bad_seq_length)
+            #print(f'ORIGINAL SEQUENCE: {sequence} -> BAD SEQUENCE: {bad_seq}')
             new_tour = create_new_tour(tour, index, bad_seq)
             new_tour_error = get_tour_error(get_tour_length(tour, distance_matrix), get_tour_length(new_tour, distance_matrix))
-            print(f'ERROR: {new_tour_error}\n')
-
-        print(permutation_list)
-        error_dict_sorted = {k: v for k, v in sorted(error_dict.items(), key=lambda item: item[1], reverse = True)}
-        worst_score_index = list(error_dict_sorted.keys())[0]
-        worst_sequences.append(permutation_list[worst_score_index])
-        print(f'{error_dict_sorted}\n\n\n')
+            error_dict[tuple(bad_seq)] = new_tour_error # {([n1, n4, n2, n3, n5]): error, ...}
+            #print(f'ERROR: {new_tour_error}\n')
+    
+    # Sort the Dictionary starting from the highest errors
+    error_dict_sorted = {k: v for k, v in sorted(error_dict.items(), key=lambda item: item[1], reverse = True)}
+    # Add the element to the list choosing them if they are suitable with the given target
+    worst_sequences = [list(seq) for seq in list(error_dict_sorted.keys())[0:len(worst_indexes)] if error_dict_sorted[seq] > ERROR_TARGET] # [[seq1], [seq2], ..., [seq5]] 
 
     return worst_sequences
 
@@ -259,7 +294,7 @@ def get_worst_sequences(tour, worst_indexes, distance_matrix, N_SEQUENCE):
 def create_new_tour(tour, index, sequence):
     #print(f'TOUR: {tour}')
     n_sequence = len(sequence)
-    print(f'Index: {index}, Sequence to Replace: {sequence}')
+    #print(f'Index: {index}, Sequence to Replace: {sequence}')
     new_tour = tour.copy()
     # Even Sequence
     if n_sequence % 2 == 0:
@@ -296,9 +331,9 @@ def get_sequence(tour, index, N_SEQUENCE):
         - distance_matrix : the matrix containing the distances of each node
 """
 def get_sorted_errors(tour, tour_length, distance_matrix):
-    error_dict = {}
+    error_dict = {} # {index: error}
     #print(f'ORIGINAL TOUR: {tour}')
-    for i in range(0, 100):
+    for i in range(0, len(tour)):
         # Create a new solution starting from the best one
         new_tour = tour.copy()
         # Exchange the i-node with its next one
@@ -308,7 +343,9 @@ def get_sorted_errors(tour, tour_length, distance_matrix):
         new_tour_length = get_tour_length(new_tour, distance_matrix)
         # Calculate the error between the new tour and the best known
         error = get_tour_error(tour_length, new_tour_length)
-        error_dict[i] = error
+        error_dict[i] = error # {index: error, ...}
+
+    # Sort the Dictionary starting from the highest errors
     error_dict_sorted = {k: v for k, v in sorted(error_dict.items(), key=lambda item: item[1], reverse = True)}
     return error_dict_sorted
 
@@ -331,6 +368,7 @@ def get_tour_length(tour, distance_matrix):
 def get_sequence_length(sequence, distance_matrix):
     total_distance = 0.0
     for idx, node in enumerate(sequence):
+        # A sequence doesn't need to close the last node with the first one
         if node != sequence[-1]:
             total_distance += round(distance_matrix[node-1][sequence[(idx+1)]-1])
     return int(total_distance)
@@ -345,14 +383,18 @@ def get_tour_error(best_tour_length, tour_length):
 
 """ Validate the Inputs
 """
-def validate_input(N_SEQUENCE, N_RECORDS):
+def validate_input(N_NODES, N_SEQUENCE, N_RECORDS, ERROR_TARGET):
     try:
-        assert N_RECORDS % 4 == 0 and 4 <= N_SEQUENCE <= 8
-    except AssertionError as error:
+        assert N_RECORDS % 4 == 0 and 4 <= N_SEQUENCE <= 8 and 2 <= ERROR_TARGET <= 5 and ((N_RECORDS//4) * N_SEQUENCE) <= N_NODES
+    except AssertionError:
         if (N_RECORDS % 4) != 0:
             print(f'Invalid Number of Records: {N_RECORDS}. Please insert Number of Records divisible by 4.')
         if 4 <= N_SEQUENCE <= 8:
             print(f'Invalid Number of Nodes in a Sequence: {N_SEQUENCE}. Please consider a number of Nodes between 4 and 8.')
+        if 2 <= N_SEQUENCE <= 5:
+            print(f'Invalid Error Target chosen: {N_SEQUENCE}. Please consider a number between 2 and 5.')
+        if ((N_RECORDS//4) * N_SEQUENCE) > N_NODES:
+            print(f'Invalid Error Inputs chosen: {N_RECORDS}, {N_SEQUENCE}. Please consider the inputs so they are ((N_RECORDS//4) * N_SEQUENCE) <= N_NODES.')
         raise
 
 def main():
@@ -360,22 +402,27 @@ def main():
     DATASET_FOLDER = './Dataset'
 
     # Default Configuration if no args are given
-    N_RECORDS = 40
+    N_NODES = 100
+    N_RECORDS = 80
     N_SEQUENCE = 5
+    ERROR_TARGET = 4
     parser = argparse.ArgumentParser(description='Dataset Configuration')
     parser.add_argument('--r', dest='record', type=int, help='Record to Save for each Graph')
     parser.add_argument('--s', dest='sequence_nodes', type=int, help='Nodes in each sequence')
+    parser.add_argument('--e', dest='error_target', type=int, help='Error target on the tour')
     args = parser.parse_args()
     # Set the Configuration Parameter
     if args.record is not None:
         N_RECORDS = args.record
     if args.sequence_nodes is not None:
         N_SEQUENCE = args.sequence_nodes
+    if args.error_target is not None:
+        ERROR_TARGET = args.error_target
     # Check if the Parameters are correct
-    validate_input(N_SEQUENCE, N_RECORDS)
+    validate_input(N_NODES, N_SEQUENCE, N_RECORDS, ERROR_TARGET)
 
-    print(f'Number of Records for each Graph: {N_RECORDS}, Number of Nodes in a Sequence: {N_SEQUENCE}')
-    create_dataset(TSP_FOLDER, DATASET_FOLDER, N_RECORDS, N_SEQUENCE)
+    #print(f'Number of Records for each Graph: {N_RECORDS}, Number of Nodes in a Sequence: {N_SEQUENCE}')
+    create_dataset(TSP_FOLDER, DATASET_FOLDER, N_RECORDS, N_SEQUENCE, ERROR_TARGET)
 
 if __name__ == "__main__":
     main()    
