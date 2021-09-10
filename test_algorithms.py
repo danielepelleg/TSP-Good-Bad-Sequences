96a43ccf134c9618c99a0dcedf31c48dc592fc6c
Daniele Pellegrini <daniele.pellegrini@studenti.unipr.it>
2021-09-11 01:55:19 +0200

    Add test_algorithms.py
    Add parser for command line tests configuration. The script tests different classification's algorithms and save the results on TSPRandomGraphs-Test/Results

--- test_algorithms.py
+++ test_algorithms.py
@@ -2,7 +2,6 @@ from CreateDataset import get_coordinates_from_file, get_solution_from_file, get
 from random import seed, randint
 from csv import writer
 from sklearn.model_selection import train_test_split
-from scipy.spatial import distance_matrix
 from itertools import permutations
 
 import numpy as np
@@ -13,14 +12,14 @@ import argparse
 
 """ Create the CSV dataset file
 """
-def create_csv_file(FOLDER_NAME, timestamp, N_RECORDS=80, N_SEQUENCE=5):
+def create_csv_file(FOLDER_NAME, timestamp, N_RECORDS, N_SEQUENCE, ERROR_TARGET):
     if not os.path.exists(FOLDER_NAME):
         os.makedirs(FOLDER_NAME)
     else:
-        if os.path.exists(f'{FOLDER_NAME}/{timestamp}_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}.csv'):
-            os.remove(f'{FOLDER_NAME}/{timestamp}_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}.csv')
+        if os.path.exists(f'{FOLDER_NAME}/Results/{timestamp}_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}_ERROR{ERROR_TARGET}.csv'):
+            os.remove(f'{FOLDER_NAME}/Results/{timestamp}_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}_ERROR{ERROR_TARGET}.csv')
     columns = ['Sequence Error', 'Tour Error','Random Forest', 'Accuracy Random Forest', 'Naive Bayes', 'Accuracy Naive Bayes', 'Ada Boost', 'Accuracy Ada Boost', 'Decision Tree', 'Accuracy Decision Tree', 'Logistic Regression', 'Accuracy Logistic Regression', 'K-Nearest Neighbors', 'Accuracy KNN']
-    with open(f'{FOLDER_NAME}/{timestamp}_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}.csv', 'w', newline='') as csv_file:
+    with open(f'{FOLDER_NAME}/Results/{timestamp}_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}_ERROR{ERROR_TARGET}.csv', 'w', newline='') as csv_file:
         # creating a csv writer object 
         csvwriter = writer(csv_file, delimiter=';')
         # writing the fields 
@@ -28,12 +27,12 @@ def create_csv_file(FOLDER_NAME, timestamp, N_RECORDS=80, N_SEQUENCE=5):
 
 """ Add a record in the CSV dataset file
 """
-def add_record_to_csv(FOLDER_NAME, timestamp, record, N_RECORDS=80, N_SEQUENCE=5):
+def add_record_to_csv(FOLDER_NAME, timestamp, record, N_RECORDS, N_SEQUENCE, ERROR_TARGET):
     new_record = []
     for r in record:
         new_r = str(r)
         new_record.append(new_r.replace(".", ","))
-    with open(f'{FOLDER_NAME}/{timestamp}_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}.csv', 'a', newline='') as csv_file:
+    with open(f'{FOLDER_NAME}/Results/{timestamp}_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}_ERROR{ERROR_TARGET}.csv', 'a', newline='') as csv_file:
         # Pass this file object to csv.writer() and get a writer object
         writer_object = writer(csv_file, delimiter=';')
     
@@ -53,27 +52,27 @@ def get_worst_permutation(tour, distance_matrix, random_node_index, tour_sequenc
     print(error_dict_sorted)
     return list(error_dict_sorted.keys())[0], list(error_dict_sorted.values())[0], list(error_dict_sorted.keys())[len(error_dict_sorted)//2], list(error_dict_sorted.values())[len(error_dict_sorted)//2]
 
-def dataset_setup(N_RECORDS=80, N_SEQUENCES=5):
+def dataset_setup(N_RECORDS, N_SEQUENCE, ERROR_TARGET):
     DATASET_FOLDER = './Dataset'
-    DATASET_NAME = f'Dataset_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCES}'
+    DATASET_NAME = f'Dataset_NRECORDS{N_RECORDS}_NSEQUENCE{N_SEQUENCE}_ERROR{ERROR_TARGET}'
     dataset_file = pd.read_csv(f'{DATASET_FOLDER}/{DATASET_NAME}.csv')
 
-    X = dataset_file.iloc[:, 0:15].values
+    X = dataset_file.iloc[:, 0:(N_SEQUENCE*3)].values
     y = dataset_file[['Good']]
 
     # Training and Test Division
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
     
     #print(len(X_train), len(y_train))
     #print(len(X_test), len(y_test))
 
     return X_train, X_test, y_train, y_test
 
-def RandomForestAlgoritm(N_RECORDS=80, N_SEQUENCES=5):
+def RandomForestAlgoritm(N_RECORDS=80, N_SEQUENCE=5, ERROR_TARGET=4):
     from sklearn.ensemble import  RandomForestClassifier
     rfm = RandomForestClassifier(n_estimators=15, oob_score=False, n_jobs=-1, 
                            random_state=101, max_features=None, min_samples_leaf=30)
-    X_train, X_test, y_train, y_test = dataset_setup(N_RECORDS, N_SEQUENCES)
+    X_train, X_test, y_train, y_test = dataset_setup(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
     rfm.fit(X_train, y_train.values.ravel())
     # print(f'\nRandom Forest:\n')
     # print("TRAIN SET", rfm.score(X_train, y_train))
@@ -84,12 +83,12 @@ def RandomForestAlgoritm(N_RECORDS=80, N_SEQUENCES=5):
     # print(f'Accuracy: {accuracy}\n')
     return rfm
 
-def NaiveBayesAlgorithm(N_RECORDS=80, N_SEQUENCES=5):
+def NaiveBayesAlgorithm(N_RECORDS=80, N_SEQUENCE=5, ERROR_TARGET=4):
     #  Random Forest Algor
     # Naive Bayes
     from sklearn.naive_bayes import GaussianNB
     nb= GaussianNB()
-    X_train, X_test, y_train, y_test = dataset_setup()
+    X_train, X_test, y_train, y_test = dataset_setup(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
     nb.fit(X_train, y_train.values.ravel())
     # print(f'\nNaive Bayes:\n')
     # print("TRAIN SET", nb.score(X_train, y_train))
@@ -100,10 +99,10 @@ def NaiveBayesAlgorithm(N_RECORDS=80, N_SEQUENCES=5):
     # print(f'Accuracy: {accuracy}\n')
     return nb
 
-def AdaBoostAlgorithm(N_RECORDS=80, N_SEQUENCES=5):
+def AdaBoostAlgorithm(N_RECORDS=80, N_SEQUENCE=5, ERROR_TARGET=4):
     from sklearn.ensemble import AdaBoostClassifier
     ada = AdaBoostClassifier(n_estimators=100, random_state=0)
-    X_train, X_test, y_train, y_test = dataset_setup()
+    X_train, X_test, y_train, y_test = dataset_setup(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
     ada.fit(X_train, y_train.values.ravel())
     # print(f'\nAdaBoost:\n')
     # print("TRAIN SET", ada.score(X_train, y_train))
@@ -114,10 +113,10 @@ def AdaBoostAlgorithm(N_RECORDS=80, N_SEQUENCES=5):
     # print(f'Accuracy: {accuracy}\n')
     return ada
 
-def DecisionTreeAlgorithm(N_RECORDS=80, N_SEQUENCES=5):
+def DecisionTreeAlgorithm(N_RECORDS=80, N_SEQUENCE=5, ERROR_TARGET=4):
     from sklearn import tree
     clf = tree.DecisionTreeClassifier()
-    X_train, X_test, y_train, y_test = dataset_setup()
+    X_train, X_test, y_train, y_test = dataset_setup(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
     clf.fit(X_train, y_train)
     # print(f'\nDecision Tree:\n')
     # print("TRAIN SET", clf.score(X_train, y_train))
@@ -128,10 +127,10 @@ def DecisionTreeAlgorithm(N_RECORDS=80, N_SEQUENCES=5):
     # print(f'Accuracy: {accuracy}\n')
     return clf
 
-def LogisticRegressionAlgorithm(N_RECORDS=80, N_SEQUENCES=5):
+def LogisticRegressionAlgorithm(N_RECORDS=80, N_SEQUENCE=5, ERROR_TARGET=4):
     from sklearn.linear_model import LogisticRegression
     lr=LogisticRegression(max_iter=1000)
-    X_train, X_test, y_train, y_test = dataset_setup()
+    X_train, X_test, y_train, y_test = dataset_setup(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
     lr.fit(X_train, y_train.values.ravel())
     # print(f'\nLogistic Regression:\n')
     # print("TRAIN SET", lr.score(X_train, y_train))
@@ -142,10 +141,10 @@ def LogisticRegressionAlgorithm(N_RECORDS=80, N_SEQUENCES=5):
     # print(f'Accuracy: {accuracy}\n')
     return lr
 
-def KNNAlgorithm(N_RECORDS=80, N_SEQUENCES=5):
+def KNNAlgorithm(N_RECORDS=80, N_SEQUENCE=5, ERROR_TARGET=4):
     from sklearn.neighbors import  KNeighborsClassifier
     knn=KNeighborsClassifier(n_neighbors=5)
-    X_train, X_test, y_train, y_test = dataset_setup()
+    X_train, X_test, y_train, y_test = dataset_setup(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
     knn.fit(X_train, y_train.values.ravel())
     # print(f'\nKNN:\n')
     # print("TRAIN SET", knn.score(X_train, y_train))
@@ -156,18 +155,18 @@ def KNNAlgorithm(N_RECORDS=80, N_SEQUENCES=5):
     # print(f'Accuracy: {accuracy}\n')
     return knn
 
-def test_algorithms(N_RECORDS, N_SEQUENCE):
+def test_algorithms(N_RECORDS, N_SEQUENCE, ERROR_TARGET):
     seed(100)
-    TSP_TEST_FOLDER = './TSPRandomGraph-Test/Results'
+    TSP_TEST_FOLDER = './TSPRandomGraph-Test'
     timestamp = time.strftime('%Y%m%d-%H.%M')
-    create_csv_file(TSP_TEST_FOLDER, timestamp, N_RECORDS, N_SEQUENCE)
+    create_csv_file(TSP_TEST_FOLDER, timestamp, N_RECORDS, N_SEQUENCE, ERROR_TARGET)
     for i in range(1, 11):
         file_name = f'problem_{i}'
         print(f'{file_name}\n')
         coords, dist_mat = get_coordinates_from_file(TSP_TEST_FOLDER, file_name)
         tour_length, tour = get_solution_from_file(TSP_TEST_FOLDER, file_name)
         random_node_index = randint(0,len(tour))
-        solution_sequence = get_sequence(tour, random_node_index, 5)
+        solution_sequence = get_sequence(tour, random_node_index, N_SEQUENCE)
         solution_sequence_length = get_sequence_length(solution_sequence, dist_mat)
         solution_record = [create_record(solution_sequence, coords, dist_mat, None)]
         solution_result = [0, 0]
@@ -181,7 +180,7 @@ def test_algorithms(N_RECORDS, N_SEQUENCE):
         worst_result = [worst_sequence_error, worst_perm_error]
         print(f'Random NODE IDX: {random_node_index}\nSOL. Sequence: {solution_sequence},\nMID SEQ: {mid_perm}\t-> Error: {mid_sequence_error}, New Tour Error:{mid_perm_error}\nWORST SEQ: {worst_perm}\t-> Error: {worst_sequence_error}, New Tour Error:{worst_perm_error}\n')
 
-        rfa = RandomForestAlgoritm()
+        rfa = RandomForestAlgoritm(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
         print('RANDOM FOREST:')
         print(f'Predict SOL  . SEQ: {rfa.predict(solution_record)} \tAccuracy: {rfa.predict_proba(solution_record)}')
         print(f'Predict MID  . SEQ: {rfa.predict(mid_perm_record)} \tAccuracy: {rfa.predict_proba(mid_perm_record)}')
@@ -193,7 +192,7 @@ def test_algorithms(N_RECORDS, N_SEQUENCE):
         worst_result.append(rfa.predict(worst_perm_record)[0])
         worst_result.append(max(rfa.predict_proba(worst_perm_record)[0]))
         
-        nba = NaiveBayesAlgorithm()
+        nba = NaiveBayesAlgorithm(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
         print('NAIVE BAYES:')
         print(f'Predict SOL  . SEQ: {nba.predict(solution_record)} \tAccuracy: {nba.predict_proba(solution_record)}')
         print(f'Predict MID  . SEQ: {nba.predict(mid_perm_record)} \tAccuracy: {nba.predict_proba(mid_perm_record)}')
@@ -205,7 +204,7 @@ def test_algorithms(N_RECORDS, N_SEQUENCE):
         worst_result.append(nba.predict(worst_perm_record)[0])
         worst_result.append(max(nba.predict_proba(worst_perm_record)[0]))
 
-        ada = AdaBoostAlgorithm()
+        ada = AdaBoostAlgorithm(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
         print('ADA BOOST:')
         print(f'Predict SOL  . SEQ: {ada.predict(solution_record)} \tAccuracy: {ada.predict_proba(solution_record)}')
         print(f'Predict MID  . SEQ: {ada.predict(mid_perm_record)} \tAccuracy: {ada.predict_proba(mid_perm_record)}')
@@ -217,7 +216,7 @@ def test_algorithms(N_RECORDS, N_SEQUENCE):
         worst_result.append(ada.predict(worst_perm_record)[0])
         worst_result.append(max(ada.predict_proba(worst_perm_record)[0]))
 
-        dta = DecisionTreeAlgorithm()
+        dta = DecisionTreeAlgorithm(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
         print('DECISION TREE:')
         print(f'Predict SOL  . SEQ: {dta.predict(solution_record)} \tAccuracy: {dta.predict_proba(solution_record)}')
         print(f'Predict MID  . SEQ: {dta.predict(mid_perm_record)} \tAccuracy: {dta.predict_proba(mid_perm_record)}')
@@ -229,7 +228,7 @@ def test_algorithms(N_RECORDS, N_SEQUENCE):
         worst_result.append(dta.predict(worst_perm_record)[0])
         worst_result.append(max(dta.predict_proba(worst_perm_record)[0]))
 
-        lra = LogisticRegressionAlgorithm()
+        lra = LogisticRegressionAlgorithm(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
         print('LOGISTIC REGRESSION:')
         print(f'Predict SOL  . SEQ: {lra.predict(solution_record)} \tAccuracy: {lra.predict_proba(solution_record)}')
         print(f'Predict MID  . SEQ: {lra.predict(mid_perm_record)} \tAccuracy: {lra.predict_proba(mid_perm_record)}')
@@ -241,7 +240,7 @@ def test_algorithms(N_RECORDS, N_SEQUENCE):
         worst_result.append(lra.predict(worst_perm_record)[0])
         worst_result.append(max(lra.predict_proba(worst_perm_record)[0]))
 
-        knn = KNNAlgorithm()
+        knn = KNNAlgorithm(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
         print('K-NEAREST NEIGHBORS:')
         print(f'Predict SOL  . SEQ: {knn.predict(solution_record)} \tAccuracy: {knn.predict_proba(solution_record)}')
         print(f'Predict MID  . SEQ: {knn.predict(mid_perm_record)} \tAccuracy: {knn.predict_proba(mid_perm_record)}')
@@ -253,25 +252,29 @@ def test_algorithms(N_RECORDS, N_SEQUENCE):
         worst_result.append(knn.predict(worst_perm_record)[0])
         worst_result.append(max(knn.predict_proba(worst_perm_record)[0]))
 
-        add_record_to_csv(TSP_TEST_FOLDER, timestamp, solution_result)
-        add_record_to_csv(TSP_TEST_FOLDER, timestamp, mid_result)
-        add_record_to_csv(TSP_TEST_FOLDER, timestamp, worst_result)
+        add_record_to_csv(TSP_TEST_FOLDER, timestamp, solution_result, N_RECORDS, N_SEQUENCE, ERROR_TARGET)
+        add_record_to_csv(TSP_TEST_FOLDER, timestamp, mid_result, N_RECORDS, N_SEQUENCE, ERROR_TARGET)
+        add_record_to_csv(TSP_TEST_FOLDER, timestamp, worst_result, N_RECORDS, N_SEQUENCE, ERROR_TARGET)
 
     print(f'-----------------------------------\n')
 
 def main():
     N_RECORDS = 80
     N_SEQUENCE = 5
+    ERROR_TARGET = 4
     parser = argparse.ArgumentParser(description='Test Configuration')
     parser.add_argument('--r', dest='records', type=int, help='Records for each graph of the dataset to use')
     parser.add_argument('--s', dest='sequence_nodes', type=int, help='Nodes in each sequence')
+    parser.add_argument('--e', dest='error_target', type=float, help='Error target on the tour')
     args = parser.parse_args()
     # Set the Configuration Parameter
-    if args.record is not None:
-        N_RECORDS = args.record
+    if args.records is not None:
+        N_RECORDS = args.records
     if args.sequence_nodes is not None:
-        N_SEQUENCE = args.sequence_node
-    test_algorithms(N_RECORDS, N_SEQUENCE)
+        N_SEQUENCE = args.sequence_nodes
+    if args.error_target is not None:
+        ERROR_TARGET = args.error_target
+    test_algorithms(N_RECORDS, N_SEQUENCE, ERROR_TARGET)
 
 if __name__ == "__main__":
     main()    
